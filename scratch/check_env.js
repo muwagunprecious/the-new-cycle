de2886ecf4d5a951c41fcd1e87315ccaf6ee5357
muwagunprecious <ademuwagunremi60@gmail.com>
2026-06-03 01:32:06 +0100

    fix: remove large files from git tracking, update .gitignore

--- scratch/check_env.js
+++
@@ -1,4 +0,0 @@
-console.log('JWT_SECRET is set:', !!process.env.JWT_SECRET);
-if (process.env.JWT_SECRET) {
-    console.log('JWT_SECRET length:', process.env.JWT_SECRET.length);
-}
--- scratch/test_contact.js
+++
@@ -1,6 +0,0 @@
-import { getContactMessages } from './backend-actions/actions/contact.js';
-
-(async () => {
-  const res = await getContactMessages();
-  console.log('Result:', res);
-})();
